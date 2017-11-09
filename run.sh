974bd4fc94f3b6e073a4b4972007b0f9b27fed22
Craig Allwardt <craig.allwardt@pnnl.gov>
2017-11-08 18:18:10 -0800

    Updated run.sh to ingest data properly and to not block so the rest of the script can run

--- run.sh
+++ run.sh
@@ -1,55 +1,52 @@
 #!/bin/bash
 
 
-viz_url="http://localhost:8080"
+viz_url="http://localhost:8080/ieee8500"
 
-
+DATA_DIR="dumps"
 
 # Mysql
-[ ! -d mysqldump ] && mkdir mysqldump
-if [ ! -f mysqldump/gridappsd_mysql_dump.sql ]; then
+[ ! -d "$DATA_DIR" ] && mkdir "$DATA_DIR"
+if [ ! -f "$DATA_DIR/gridappsd_mysql_dump.sql" ]; then
   echo " "
   echo "Downloading mysql data"
-  curl -s -o mysqldump/gridappsd_mysql_dump.sql https://raw.githubusercontent.com/GRIDAPPSD/Bootstrap/master/gridappsd_mysql_dump.sql
-  sed -i -e "s/'gridappsd'@'localhost'/'gridappsd'@'%'/g" mysqldump/gridappsd_mysql_dump.sql
+  curl -s -o "$DATA_DIR/gridappsd_mysql_dump.sql" "https://raw.githubusercontent.com/GRIDAPPSD/Bootstrap/master/gridappsd_mysql_dump.sql"
+  sed -i -e "s/'gridappsd'@'localhost'/'gridappsd'@'%'/g" $DATA_DIR/gridappsd_mysql_dump.sql
 fi
 
-# Docker
-echo " "
-echo "Starting docker containers"
+if [ ! -f "$DATA_DIR/ieee8500.xml" ]; then
+  echo " "
+  echo "Downloading blazegraph data"
+  curl -s -o "$DATA_DIR/ieee8500.xml" "https://raw.githubusercontent.com/GRIDAPPSD/Bootstrap/master/ieee8500.xml"
+fi
+
+
+status=$(curl -s --head -w %{http_code} "http://localhost:8889/bigdata/" -o /dev/null)
 
 docker-compose up -d
 
+while [ $status -ne "200" ]
+do
+  status=$(curl -s --head -w %{http_code} "http://localhost:8889/bigdata/" -o /dev/null)
+done
 
-# Blazegraph
-if [ ! -f ieee8500.xml ]; then
-  echo " "
-  if [ `docker-compose ps | grep -c "gridappsddocker_blazegraph"` -gt 0 ]; then
-    echo "Downloading and importing blazegraph data"
-  
-    curl -s -o ieee8500.xml https://raw.githubusercontent.com/GRIDAPPSD/Bootstrap/master/ieee8500.xml
-    output=`curl -s -D- -H 'Content-Type: application/xml' --upload-file ieee8500.xml -X POST 'http://localhost:8889/bigdata/sparql'`
+# sleep just a little longer to make sure blazegraph is ready to receive data.
+sleep 3
+echo "Ingesting blazegraph data"
+output=`curl -s -D- -H 'Content-Type: application/xml' --upload-file "$DATA_DIR/ieee8500.xml" -X POST 'http://localhost:8889/bigdata/sparql'`
+echo "Finished uploading blazegraph data"
 
-    if [ `echo $output | grep -c 'data modified="446127"'` -lt 1 ]; then
-      echo "Blazegraph import failed"
-      echo $output
-    else
-      echo "Blazegraph data imported"
-    fi
-  else
-    echo "Error connecting with blazegraph container"
-  fi
-  echo " "
+status="0"
+while [ $status -ne "200" ]
+do
+  status=$(curl -s --head -w %{http_code} "$viz_url" -o /dev/null)
+done
+
+echo "Opening web browser to the viz container $viz_url"
+if [ `uname` == "Linux" ]; then
+ xdg-open $viz_url
+elif [ `uname` == "Darwin" ]; then
+ open $viz_url
+else
+ echo "Please open a browser to $viz_url"
 fi
-
-
-
-#echo " "
-#echo "Opening web browser to the viz container $viz_url"
-#if [ `uname` == "Linux" ]; then
-#  xdg-open $viz_url
-#elif [ `uname` == "Darwin" ]; then
-#  open $viz_url
-#else
-#  echo "Please open a browser to $viz_url"
-#fi
