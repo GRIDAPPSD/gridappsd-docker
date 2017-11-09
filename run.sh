340c451f54946362eb968de0db03628cbf1c038c
Tonya Martin <tonya.martin@pnnl.gov>
2017-11-09 12:53:59 -0800

    updated ENV variables

--- run.sh
+++ run.sh
@@ -23,6 +23,8 @@ fi
 
 status=$(curl -s --head -w %{http_code} "http://localhost:8889/bigdata/" -o /dev/null)
 
+echo " "
+echo "Starting docker containers"
 docker-compose up -d
 
 while [ $status -ne "200" ]
@@ -32,6 +34,7 @@ done
 
 # sleep just a little longer to make sure blazegraph is ready to receive data.
 sleep 3
+echo " "
 echo "Ingesting blazegraph data"
 output=`curl -s -D- -H 'Content-Type: application/xml' --upload-file "$DATA_DIR/ieee8500.xml" -X POST 'http://localhost:8889/bigdata/sparql'`
 echo "Finished uploading blazegraph data"
@@ -42,11 +45,13 @@ do
   status=$(curl -s --head -w %{http_code} "$viz_url" -o /dev/null)
 done
 
+echo " "
 echo "Opening web browser to the viz container $viz_url"
 if [ `uname` == "Linux" ]; then
  xdg-open $viz_url
 elif [ `uname` == "Darwin" ]; then
  open $viz_url
 else
- echo "Please open a browser to $viz_url"
+  echo " "
+  echo "Please open a browser to $viz_url"
 fi
